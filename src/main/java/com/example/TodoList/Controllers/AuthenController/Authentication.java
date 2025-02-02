18db83e6f463e0ec3bf0539e0c881b7aa70631d8
WokCao <henryquoccao@gmail.com>
2025-02-02 12:57:05 +0700

    Create: post request to register account

--- src/main/java/com/example/TodoList/Controllers/AuthenController/Authentication.java
+++ src/main/java/com/example/TodoList/Controllers/AuthenController/Authentication.java
@@ -1,5 +1,9 @@
 package com.example.TodoList.Controllers.AuthenController;
 
+import com.example.TodoList.DTOs.AuthenDTO.Register;
+import com.example.TodoList.Models.UserModel.User;
+import com.example.TodoList.Services.AuthenService.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +12,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/authentication")
 public class Authentication {
-    @PostMapping("/test")
-    public ResponseEntity<?> test(@RequestBody Map<String, String> testObj) {
-        System.out.println(testObj.get("value"));
+    @Autowired
+    private RegisterService registerService;
+    @PostMapping("/register")
+    public ResponseEntity<?> registerAccount(@RequestBody(required = true) Register registerObject) {
+        User user = registerService.registerUser(registerObject);
+        System.out.println(user);
         return ResponseEntity.ok().build();
     }
 }
